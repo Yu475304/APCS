cf0ae4c8024f656b975a8bf17dfd8ae013e28887
Yu475304 <s1130139@dmhs.kh.edu.tw>
2026-05-26 11:58:25 +0800

    a065提款卡密碼.cpp

---
+++ a065提款卡密碼.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string S;
+	cin >> S;
+	for (int i = 0; i < s,size() - 1; i++){
+		int distant = abs (s[i] - s[i-1]);
+		cout << distant;
+	}
+	cout << endl;
+	return 0;
+}
